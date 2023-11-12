d98ffbc8c7084fbf65a6ef879750dcb1f6d14f81
Nizar4790k <nizar4790k@gmail.com>
2023-11-12 03:58:04 -0400

    added duck

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+l
+<app/src/main/java/com/example/jetpackcompose/MainActivity.kt,6\1\615b6418a5ba631190e56006575cfd35d4e12b50
--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,31 +1,50 @@
 package com.example.jetpackcompose
 
+
+
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+
+
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val painter = painterResource(id = R.drawable.duck)
+            val description = "Duck "
+            val title = "Yellow Duck"
+
+            ImageCard(painter = painter, contentDescrition = description, title = title)
+
+            /*
             Row(
                 modifier = Modifier
                     .width(300.dp)
@@ -40,7 +59,7 @@ class MainActivity : ComponentActivity() {
                 Text(text="Hello")
             }
 
-            /*
+
             JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -71,4 +90,51 @@ fun GreetingPreview() {
     JetpackComposeTheme {
         Greeting("Android")
     }
+}
+
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescrition:String,
+    title:String,
+    modifier:Modifier = Modifier
+){
+     Card(
+
+         modifier=modifier.fillMaxWidth(),
+         shape = RoundedCornerShape(15.dp),
+         elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+
+     ){
+
+         Box (modifier=Modifier.height(200.dp)){
+
+
+             Image(painter,contentDescrition,Modifier, Alignment.Center, ContentScale.Crop)
+
+             Box(modifier=Modifier
+                 .fillMaxSize()
+                 .background(
+                     Brush.verticalGradient(
+                         colors = listOf(
+                             Color.Transparent,
+                             Color.Black
+                         ),
+
+                         startY= 300f
+                     )
+                 ))
+
+             Box(modifier = Modifier
+                 .fillMaxSize()
+                 .padding(12.dp)){
+
+                 Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                 
+             }
+
+         }
+
+     }
 }
