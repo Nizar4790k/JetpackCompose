7722e5ff505c72cb480b7853256319b4f41bb27f
Nizar4790k <nizar4790k@pop-os.localdomain>
2024-06-22 17:44:03 -0400

    added new fonts

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -30,7 +30,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,22 +52,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val scafoldState  = rememberScaffoldState()
-            
-           Scaffold(modifier = Modifier.fillMaxSize(),
-                    scaffoldState  =
-               ) {
+            val fontFamily = FontFamily(
+                Font(R.font.lexend_thin,FontWeight.Thin),
+                Font(R.font.lexend_light,FontWeight.Light),
+                Font(R.font.lexend_regular,FontWeight.Normal),
+                Font(R.font.lexend_medium,FontWeight.Medium),
+                Font(R.font.lexend_semibold,FontWeight.SemiBold),
+                Font(R.font.lexend_bold,FontWeight.Bold),
+                Font(R.font.lexend_extrabold,FontWeight.Light),
 
-           }
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(color = 0xFFF101010))
+            ) {
+                Text(
+                    text = buildAnnotatedString
+                    { withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)
+                    ) {
+                        append("J")
+                    }
+                        append("epack ")
+
+                        withStyle(style = SpanStyle(color = Color.Green, fontSize = 50.sp)
+                        ) {
+                            append("C")
+                        }
+
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
+            }
 
             /*
+
             val painter = painterResource(id = R.drawable.duck)
             val description = "Duck "
             val title = "Yellow Duck"
 
 
             ImageCard(painter = painter, contentDescrition = description, title = title)
-  */
+
+            */
+
 
             /*
             Row(
