9b9a3046da3094a54cb5c787a21d1a8b70bf1e72
Nizar4790k <nizar4790k@pop-os.localdomain>
2024-06-22 18:51:42 -0400

    status finished

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -8,9 +8,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,31 +19,22 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
 
@@ -52,6 +43,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
+            val color = remember { mutableStateOf(Color.Yellow) }
+
+            Column(Modifier.fillMaxSize()){
+                ColorBox(
+                    Modifier.weight(1f).fillMaxSize()
+                ){
+                    color.value = it
+                }
+
+                Box(modifier = Modifier
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()
+                )
+            }
+
+
+
+            /*
             val fontFamily = FontFamily(
                 Font(R.font.lexend_thin,FontWeight.Thin),
                 Font(R.font.lexend_light,FontWeight.Light),
@@ -93,6 +103,8 @@ class MainActivity : ComponentActivity() {
                 )
             }
 
+             */
+
             /*
 
             val painter = painterResource(id = R.drawable.duck)
@@ -132,6 +144,22 @@ class MainActivity : ComponentActivity() {
 
              */
             }
+        }
+
+    @Composable
+    fun ColorBox(modifier: Modifier, updatedColor:(Color)->Unit){
+
+        Box(modifier=modifier
+            .clickable {
+                updatedColor(Color(
+                Random.nextFloat(),
+                Random.nextFloat(),
+                Random.nextFloat(),
+                1f
+            ))
+
+            }
+            )
         }
     }
 
@@ -198,4 +226,7 @@ fun ImageCard(
          }
 
      }
+
+
+
 }
